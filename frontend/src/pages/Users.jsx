5a43435e61ebefac19de0e188b2caa84a49e7133
Gerardbulky <gerardambe@yahoo.com>
2024-11-25 19:31:19 +0100

    Made some changes

--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 // const API_URL = import.meta.env.VITE_API_URL;
-const API_URL = "http://52.66.249.115:30005";
+const API_URL = "http://app.example";
 
 console.log('API URL:', API_URL); 
 
