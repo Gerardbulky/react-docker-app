636761c647cf750c839422742340440c1425c56e
Gerardbulky <gerardambe@yahoo.com>
2024-11-25 19:32:41 +0100

    modified:   ../backend/app.py 	modified:   ../manifest/mongo-manifest/backend-deployment.yaml 	modified:   ../manifest/mongo-manifest/frontend-deployment.yaml

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ app.config["MONGO_URI"] = mongo_uri
 app.secret_key = os.environ.get("SECRET_KEY")
 
 # CORS(app, resources={r"/*": {"origins": "http://52.66.249.115:30005"}}, methods=["GET", "POST"], supports_credentials=True, allow_headers=["Content-Type"])
-CORS(app, origins=["http://52.66.249.115:30001"], methods=["GET", "POST"], supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "http://app.example"}}, methods=["GET", "POST"], supports_credentials=True)
 
 
 mongo = PyMongo(app)
