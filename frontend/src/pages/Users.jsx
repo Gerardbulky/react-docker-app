890fd680290af11c977dadd8718fc2536267c08e
Gerardbulky <gerardambe@yahoo.com>
2024-11-27 11:35:22 +0100

    Some Chnages

--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 // const API_URL = import.meta.env.VITE_API_URL;
-const API_URL = "http://13.233.91.237:5000";
+const API_URL = "http://13.233.91.237:30005";
 
 console.log('API URL:', API_URL); 
 
